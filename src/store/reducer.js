fbe0262ee6824790d9c30cbd4c5033177445c9d5
xyh123321 <376564838@qq.com>
2020-08-03 22:59:48 +0800

    [redux][jayden] refactor: when decrease,value-1 instead of +1 in the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,11 @@
 import { INCREASE, DECREASE } from '../action/Action';
 
 function reducer(state = { value: 0 }, action) {
-    const value = state.value
     switch (action.type) {
         case INCREASE:
-            return { value: value + 1 }
+            return {...state, value: state.value + 1 }
         case DECREASE:
-            return { value: value + 1 }
+            return {...state, value: state.value - 1 }
         default:
             return state
     }
